c77522f90b0a669f1391cc9059ac3b2ad6ec1d23
Austin Spraggins <atmosphere9999@gmail.com>
2024-12-31 22:02:32 -0800

    Update .gitignore to streamline ignored files
    - Removed specific config files from being ignored to allow for better tracking of configuration changes.

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+	supabaseUrl: "https://xdkjefgcsxewhpueykvc.supabase.co",
+	supabaseKey:
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhka2plZmdjc3hld2hwdWV5a3ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MDkwNzksImV4cCI6MjA1MTI4NTA3OX0.3HgHF9llAUO8o6futwYu9eRLuMHw792RbhYb-KyLNxE"
+};
+
+window.config = config;
